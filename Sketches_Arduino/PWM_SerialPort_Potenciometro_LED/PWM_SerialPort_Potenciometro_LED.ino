6b5d3ecbd72b6deff0a099098f18d861aa46e804
Israel Zuñiga <israelzdlm@gmail.com>
2014-02-13 00:47:14 -0600

    Agregado Sketch para PWM-ADC

---
+++ Sketches_Arduino/PWM_SerialPort_Potenciometro_LED/PWM_SerialPort_Potenciometro_LED.ino
@@ -0,0 +1,17 @@
+int ledPin = 9;      // LED conecado al Pin digital #9
+int analogPin = 3;   // Porenciometro conectado al pin anag. #3
+int val = 0;         // Variable para almacenar la lectura del potenciometro
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);   // establecemos el pin "ledPIN" en salida
+}
+
+void loop()
+{
+  val = analogRead(analogPin);   // leemos el potenciometro
+  analogWrite(ledPin, val / 4);  // los valores en analogRead van de 0-1023 , en analogWrite van de 0-255
+  Serial.println("El valor ahora es: ");
+  Serial.println(val);
+}
